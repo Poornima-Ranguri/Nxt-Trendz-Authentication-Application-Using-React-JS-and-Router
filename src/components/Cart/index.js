0186240f00e43d2f8b04cdbe2f965cef76f62996
Poornima-Ranguri <poornimaranguri@gmail.com>
2024-04-05 17:28:49 +0000

    Nxt Trendz Authentication Using React JS and Router

---
+++ src/components/Cart/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const Cart = () => (
+  <div className="cart-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      className="cart-image"
+      alt="cart"
+    />
+  </div>
+)
+
+export default Cart
