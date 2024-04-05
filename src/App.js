4a65c6bcc8bb29ea91da03128d1bd854f0df2fb0
Poornima-Ranguri <poornimaranguri@gmail.com>
2024-04-05 18:21:18 +0000

    Nxt Trend Authentication Application Using React JS and Routers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import {Route, Switch} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 
 import LoginForm from './components/LoginForm'
 import Home from './components/Home'
 import Products from './components/Products'
-
 import Cart from './components/Cart'
 import NotFound from './components/NotFound'
 
@@ -14,8 +13,7 @@ const App = () => (
     <Route exact path="/login" component={LoginForm} />
     <Route exact path="/" component={Home} />
     <Route exact path="/products" component={Products} />
-    <Route exact path="/Cart" component={Cart} />
-
+    <Route exact path="/cart" component={Cart} />
     <Route component={NotFound} />
   </Switch>
 )
--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,6 +1,7 @@
 import {Component} from 'react'
-import Cookies from 'js-cookie'
 import {Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
 import './index.css'
 
 class LoginForm extends Component {
@@ -21,8 +22,10 @@ class LoginForm extends Component {
 
   onSubmitSuccess = jwtToken => {
     const {history} = this.props
-    Cookies.set('jwt_token', jwtToken, {expires: 30})
 
+    Cookies.set('jwt_token', jwtToken, {
+      expires: 30,
+    })
     history.replace('/')
   }
 
@@ -41,6 +44,7 @@ class LoginForm extends Component {
     }
     const response = await fetch(url, options)
     const data = await response.json()
+
     if (response.ok === true) {
       this.onSubmitSuccess(data.jwt_token)
     } else {
@@ -91,6 +95,7 @@ class LoginForm extends Component {
   render() {
     const {showSubmitError, errorMsg} = this.state
     const jwtToken = Cookies.get('jwt_token')
+
     if (jwtToken !== undefined) {
       return <Redirect to="/" />
     }
